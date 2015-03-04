98152216b2607e5d13c8628b8255275c2c15e43c
Chris Thompson <chris.thompson@govdelivery.com>
2015-03-03 22:19:46 -0600

    add initial consul data

--- src/clj/consulate/handler.clj
+++ src/clj/consulate/handler.clj
@@ -8,12 +8,54 @@
             [prone.middleware :refer [wrap-exceptions]]
             [environ.core :refer [env]]))
 
-(defn say-hi [request]
-  (response {:body "hello world"}))
+(defn get-leader [request]
+  (response {:body "10.1.10.12:8300"} ))
+
+(defn get-peers [request]
+  (response {:body ["10.1.10.12:8300",
+                    "10.1.10.11:8300",
+                    "10.1.10.10:8300"]}))
+
+(defn get-key [request]
+  (response {:body [
+                    {
+                     :CreateIndex 100,
+                     :ModifyIndex 200,
+                     :LockIndex 200,
+                     :Key "zip",
+                     :Flags 0,
+                     :Value "dGVzdA==",
+                     :Session "adf4238a-882b-9ddc-4a9d-5b6758e4159e"
+                     }
+                    ]}))
+;;...... ?recurse , ?keys , ?raw
+;; put key, body
+;; delete key
+
+;; catalog
+;; register
+;; deregister
+;; datacenters
+;; nodes
+;; services
+;; service/<service>
+;; nodes/<node>
+
+;; events
+;; fire/<name>
+;; list
+
+;; health...
+
 
 (defroutes api-routes
   (context "/api" []
-    (GET "/say" [] say-hi)))
+    (context "/v1" []
+      (context "/status" []
+        (GET "/leader" [] get-leader)
+        (GET "/peers" [] get-peers))
+      (context "/kv" []
+        (GET "/:key" [] get-key)))))
 
 (defroutes site-routes
   (GET "/" [] (render-file "templates/index.html" {:dev (env :dev?)}))
